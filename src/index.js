69463030fbb473dd1803a956df9e8d7aa388ae86
serkan <serkandemirel0420@gmail.com>
2020-03-15 02:29:58 +0300

    random and all functions implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 const names = require('./list.json');
+
+module.export = {
+  random: random,
+  allNames: allNames
+};
+
+function random() {
+  return names[Math.floor(Math.random() * names.length)];
+}
+
+function allNames() {
+  return name;
+}
+
+console.log(random());
